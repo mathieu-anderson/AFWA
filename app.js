fe9a4838e99a8e52462ceadb128393ab9c0e6681
mathieu-anderson <mathieu.hj.anderson@gmail.com>
2017-04-24 15:44:51 +1200

    Add CORS header

--- app.js
+++ app.js
@@ -8,6 +8,13 @@ const dotenv = require('dotenv').config()
 
 const app = express();
 
+//CORS header to allow requests
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
 // view engine setup
 // app.set('views', path.join(__dirname, 'views'));
 // app.set('view engine', 'hbs');
