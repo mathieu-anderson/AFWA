d619b90855d63b5834d514b124c3d40d0bc84082
mathieu-anderson <mathieu.hj.anderson@gmail.com>
2017-04-24 15:26:32 +1200

    Delete the views in the basic express setup

--- app.js
+++ app.js
@@ -9,8 +9,8 @@ const dotenv = require('dotenv').config()
 const app = express();
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'hbs');
+// app.set('views', path.join(__dirname, 'views'));
+// app.set('view engine', 'hbs');
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
